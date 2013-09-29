f6fae725c91a2e9641dc4795f15ba33f5e28a0d2
cab404 <me@cab404.ru>
2013-09-29 19:05:41 +0400

    ???????? ????? Google Play.

--- matrix/src/com/cab404/matrix/Luna.java
+++ matrix/src/com/cab404/matrix/Luna.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.Gdx;
  * @author cab404
  */
 public class Luna {
-    public static void log(Object toLog){
+    public static boolean DEBUG = true;
+
+    public static void log(Object toLog) {
+        if (!DEBUG) return;
         Gdx.app.log("Luna Log", toLog != null ? toLog.toString() : "NULL");
     }
 }
